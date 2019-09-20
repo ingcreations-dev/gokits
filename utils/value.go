03f4ead485c2bca86e69c4405f3b9c0dfbe39e18
rod_liu <rod.liu@ingcreations.com>
2019-09-20 10:26:43 +0800

    增加获取当前毫秒通用函数

--- utils/value.go
+++ utils/value.go
@@ -1,5 +1,7 @@
 package utils
 
+import "time"
+
 type value struct{
 	Code uint `json:"code"`
 	Data interface{} `json:"data"`
@@ -10,4 +12,8 @@ func GetValue(code uint,data interface{}) *value{
 	v.Code = code
 	v.Data = data
 	return v
+}
+
+func GetTimeMillis() int64 {
+	return time.Now().UnixNano() / 1e6
 }
